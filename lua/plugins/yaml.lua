79a74785ac5d170ffdd0c2c17bb66d0feb30a8a1
jUnG3 <jUnG3@users.noreply.github.com>
2025-12-18 13:57:44 +0100

    Adds yml support

---
+++ lua/plugins/yaml.lua
@@ -0,0 +1,42 @@
+return {
+  -- YAML Language Server
+  {
+    "neovim/nvim-lspconfig",
+    optional = true, -- in case you already load it elsewhere
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.lsp.config("yamlls", {
+        capabilities = capabilities,
+        settings = {
+          yaml = {
+            validate = true,
+            hover = true,
+            completion = true,
+
+            -- Pull lots of common schemas automatically
+            schemaStore = {
+              enable = true,
+              url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+
+            -- Spring Boot: treat application*.yml as Spring Boot config
+            schemas = {
+              ["https://json.schemastore.org/spring-boot-application.json"] = {
+                "application.yml",
+                "application.yaml",
+                "application-*.yml",
+                "application-*.yaml",
+                "bootstrap.yml",
+                "bootstrap.yaml",
+              },
+            },
+          },
+        },
+      })
+
+      vim.lsp.enable("yamlls")
+    end,
+  },
+}
+
